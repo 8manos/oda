ba35f665a6c0b2436907c67b82b768304b1885e3
sunny <sunny@runwalsoft.com>
2015-10-01 19:37:35 +0530

    styles added for parques page

--- wp-content/themes/sundance-child/archive.php
+++ wp-content/themes/sundance-child/archive.php
@@ -15,19 +15,22 @@ get_header(); ?>
 		<br/><br/>
 		<div class="container">
 			<div class="row">
-			<div class="col-lg-1"></div>
+			<div class="col-lg-10 col-lg-offset-1">
 		  <?php 
 					query_posts($query_string . '&orderby=date&order=ASC');
 		    while ( have_posts() ) : the_post(); ?>
-					<div class="col-lg-3 par">
-						<?php the_post_thumbnail('large', array('class' => 'img-responsive')); ?>
-						<div class="par-info">
-							<span> <?php	the_title(); ?></span>
-							<?php the_content(); ?>
+					<div class="col-lg-4 ">
+					<div  class="parq-box">
+						<?php the_post_thumbnail('full', array('class' => 'img-responsive')); ?>
+						<div class="parq-info">
+							<span class="parq-title"> <?php	the_title(); ?></span>
+							<span class="parq-content"><?php the_content(); ?></span>
+							<hr/>
 						</div> 
 					</div>
+					</div>
 				<?php endwhile; ?>
-				<div class="col-lg-1"></div>
+				</div>
 			</div>	
 		</div>
 <?php get_footer(); ?>
