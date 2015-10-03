447f6d4c3cb631fa65c7ffcae15507dda9202384
sunny <sunny@runwalsoft.com>
2015-10-03 18:26:02 +0530

    template added for custom product page

---
+++ wp-content/themes/sundance-child/archive-customproduct.php
@@ -0,0 +1,44 @@
+<?php get_header(); ?>
+  <div class="eco-banner text-center">
+			<div class="banner-txt"><span>ECO</span><p>TIENDA</p></div>
+	</div>
+	<br/><br/>
+<div class="container ecotinda">
+				<?php 
+				$i=0;
+				while ( have_posts() ) : the_post(); ?>
+					<div class="col-lg-4 prod-box">
+						<div class="prod-img text-center">
+						  <?php the_post_thumbnail('medium', array('class' => 'img-responsive')); ?>
+						</div>  
+						<div class="prod-name"><?php the_title(); ?></div>
+						<div class="prod-price"><?php echo get_post_meta($post->ID, 'price', true); ?></div>
+						<h3 class="buy_prod" data-toggle="modal" data-target="#myModal<?php echo $i; ?>"><span>Comprar &nbsp;&nbsp;<span class="buy-img"></span></span></h3>
+						<div class="modal fade" id="myModal<?php echo $i; ?>" role="dialog">
+						<div class="modal-dialog modal-lg">
+									<div class="modal-content">
+										<div class="modal-body">
+											<div class="clearfix">
+											  <div class="col-lg-12 head row">CANASTO DE COMPRA 
+												  <img src="<?php echo get_stylesheet_directory_uri();?>/img/modal-close.png" data-dismiss="modal" class="pull-right">
+												</div> 
+												<div class="col-lg-4 pimg">
+												  <?php the_post_thumbnail('medium', array('class' => 'img-responsive')); ?>
+												</div>
+												<div class="col-lg-8 pleft">
+														<div class="prod-name"><?php the_title(); ?> </div>
+					                 	<div class="prod-price"><?php echo get_post_meta($post->ID, 'price', true); ?></div>
+					                 	<div> DESCRIPCIÓN </div> 
+					                 	<div> <?php the_content(); ?></div>
+												</div>
+												<?php echo do_shortcode("[contact-form-7 id='205' title='eco tienda form']"); ?>
+											</div>	
+										</div>
+									</div>
+								</div>
+				  	</div>
+					</div>
+					<?php $i++; ?>
+				<?php endwhile; ?>
+</div>
+<?php get_footer(); ?>
