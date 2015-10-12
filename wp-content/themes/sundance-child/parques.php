442c0f257c73130f7e1e3289a0b49084c9064bf3
Manish Runwal <manish@runwalsoft.com>
2015-10-12 15:45:30 +0530

    some language conversion.

--- wp-content/themes/sundance-child/parques.php
+++ wp-content/themes/sundance-child/parques.php
@@ -12,42 +12,37 @@
 
 get_header(); ?>
 		<div class="parques-banner text-center">
-			<div class="banner-txt"><span><?php echo __('PARQUES', 'sundance'); ?></span></div>
+			<div class="banner-txt"><span><?php echo __('Parks', 'sundance'); ?></span></div>
 		</div>
 		<br/><br/>
 		<div class="container">
 			<div class="grid col-lg-offset-1 col-lg-10"> <!--style="border:1px solid red;">-->
 		  <?php
-		  $slug=array('games','recomienda','cat_workshop');
-		  foreach($slug as $sl){;
-		  $category = get_category_by_slug($sl);
-      $cid[]=$category->term_id;
-		  }
 				$args=array(
-					'category__not_in' => $cid,
+					//'category__not_in' => $cid,
 					'post_type' => 'post',
 					'post_status' => 'publish',
 					'posts_per_page' => -1,
 				);
 				$my_query = null;
 				$my_query = new WP_Query($args);
-											
+
 				$i=0;
 		    while ( $my_query->have_posts() ) : $my_query->the_post(); ?>
 					<?php if($i==2) { ?>
 						<div class="col-lg-4 grid-item " style="width:300px;">
 							<div class="catbox1">
 								<div class="indiv1">
-									<p><?php echo __('NUEYSTROS PROYECTOS', 'sundance'); ?></p>
+									<p><?php echo __('NUEYSTROS PROJECTS', 'sundance'); ?></p>
 									<ul class="list-unstyled">
-								<?php $args = array('exclude'=>$cid,'hide_empty' => FALSE,'parent' => 0); 
+								<?php $args = array('exclude'=>$cid,'hide_empty' => FALSE,'parent' => 0);
 											$categories =get_categories( $args );
 											foreach($categories as $category){
 													echo '<li><a href="'.get_category_link( $category->term_id ).'">+&nbsp;'.$category->name.'</a></li>';
 											}?>
 									</ul>
-								</div>	
-						</div>	
+								</div>
+						</div>
 					</div>
 					<?php } ?>
 					<div class="col-lg-4 grid-item " style="width:300px;">
@@ -55,18 +50,18 @@ get_header(); ?>
 						<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('full', array('class' => 'img-responsive parq-img')); ?></a>
 						<div class="parq-info">
 							<a href="<?php the_permalink(); ?>"><span class="parq-title"> <?php	the_title(); ?></span></a>
-							<div class="parq-content"><?php the_excerpt(); ?> <a class="more-link" href="<?php the_permalink(); ?>">VER MAS+</a></div>
+							<div class="parq-content"><?php the_excerpt(); ?> <a class="more-link" href="<?php the_permalink(); ?>"><?php echo __('View More','sundance'); ?>+</a></div>
 							<br/>
 							<div class="parq-date"><img src="<?php echo get_stylesheet_directory_uri();?>/img/btn-arrow.png"> <?php echo get_the_date(); ?></div>
 						</div>
 					</div>
 					</div>
-				<?php 
+				<?php
 				$i++;
 				endwhile; ?>
 			</div>
 			<div class="parq-btn text-center col-lg-12 clearfix">
-			<span><?php echo __('? QUiERES HACER UNA DONACION','sundance') ?></span>
+			<span><?php echo __('You want to make a donation?','sundance') ?></span>
 			</div>
 		</div>
 <?php get_footer(); ?>
