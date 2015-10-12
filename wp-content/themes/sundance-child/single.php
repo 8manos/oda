62e372c11d48cc006bb6bd809f8c6e7c3977a5cc
Manish Runwal <manish@runwalsoft.com>
2015-10-12 17:47:22 +0530

    Language conversion.

--- wp-content/themes/sundance-child/single.php
+++ wp-content/themes/sundance-child/single.php
@@ -8,16 +8,16 @@
 
 get_header(); ?>
   <div class="parques-banner text-center">
-		<div class="banner-txt"><span>PARQUES</span></div>
+		<div class="banner-txt"><span><?php echo __('Parks', 'sundance'); ?></span></div>
 	</div>
 	<div class="masonary_flex">
 		<?php
 				$images = get_attached_media('image', $post->ID);
-				foreach($images as $img) { 
+				foreach($images as $img) {
 				$img_src=wp_get_attachment_image_src($img->ID,'medium'); ?>
 							<img  class="" src="<?php echo $img_src[0]; ?>" alt="" />
 				<?php
-				} 
+				}
 		?>
 	</div>
 	<div class="container sp-page">
@@ -27,22 +27,22 @@ get_header(); ?>
 						<h1 class="sp-head"><?php the_title(); ?></h1>
 						<p class="sp-date"> <span><?php echo get_the_date(); ?></span></p>
 						<p class="sp-cat">
-								<?php $cat=get_the_category($post->ID); 
+								<?php $cat=get_the_category($post->ID);
 								foreach($cat as $cat_name){
 										$cats[]='<a href="'.get_category_link( $cat_name->term_id ).'">'.$cat_name->name.'</a>';
 								}
-							echo __('THEMES : ', 'sundance');	
+							echo __('THEMES : ', 'sundance');
 							echo implode(' / ',$cats);
 							?>
 						</p>
 						<div class="sp-content"><?php the_content(); ?></div>
 					<?php endwhile; ?>
-					
-					<span class="coment">COMENTAR</span><br/><br/>
+
+					<span class="coment"><?php echo __('Comments', 'sundance'); ?></span><br/><br/>
 		      <?php echo do_shortcode("[contact-form-7 id='185' title='single post page']"); ?>
 				</div>
 				<div class="col-lg-4 clearfix">
-					<?php 
+					<?php
 					$slug=array('games','recomienda','cat_workshop');
 					foreach($slug as $sl){;
 					$category = get_category_by_slug($sl);
@@ -50,10 +50,10 @@ get_header(); ?>
 					} ?>
 				<div class="catbox1">
 					<div class="indiv1">
-						<p><?php echo __('NUEYSTROS PROYECTOS', 'sundance'); ?></p>
+						<p><?php echo __('NUEYSTROS PROJECTOS', 'sundance'); ?></p>
 						<ul class="list-unstyled">
 							<?php
-							$args = array('exclude'=>$cid,'hide_empty' => FALSE,'parent' => 0); 
+							$args = array('exclude'=>$cid,'hide_empty' => FALSE,'parent' => 0);
 							$categories =get_categories( $args );
 							foreach($categories as $category){
 								echo '<li><a href="'.get_category_link( $category->term_id ).'">+&nbsp;'.$category->name.'</a></li>';
@@ -66,7 +66,7 @@ get_header(); ?>
 		<div class="clearfix row col-lg-12">
 		<br/>
 		<br/>
-		
-		</div> 
+
+		</div>
 	</div>
 <?php get_footer(); ?>
