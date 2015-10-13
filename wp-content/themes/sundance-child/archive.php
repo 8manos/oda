5e178dcd49c06ad32f54f7c1dd3af931820faccc
Prajakta Kathe <prajakta@runwalsoft.com>
2015-10-13 16:27:59 +0530

    category box condition corrected to displayed on archieve page

--- wp-content/themes/sundance-child/archive.php
+++ wp-content/themes/sundance-child/archive.php
@@ -21,36 +21,56 @@ get_header(); ?>
 				
 		  <?php
 
-				$i=0;
-		    while (have_posts() ) :the_post(); ?>
-					<?php if($i==2) { ?>
+				$i=1; wp_count_posts();
+		   while ( have_posts() ) : the_post(); ?>
+					<div class="col-lg-4 grid-item " style="width:300px;">
+						<div  class="parq-box">
+							<a href="<?php the_permalink(); ?>">
+								<?php
+
+								/*
+
+								if(get_video_thumbnail(get_the_ID() )!=""){
+										$video_thumbnail = get_video_thumbnail(get_the_ID()) ;
+									?>
+									<img class="img-responsive" src="<?php echo get_video_thumbnail(get_the_ID()); ?>" />
+								<?php
+								}else{
+									the_post_thumbnail('full', array('class' => 'img-responsive parq-img'));
+								}
+
+								*/
+
+								?>
+
+							</a>
+							<div class="parq-info">
+								<a href="<?php the_permalink(); ?>"><span class="parq-title"> <?php	the_title(); ?></span></a>
+								<div class="parq-content"><?php echo get_the_excerpt(); ?> <a class="more-link" href="<?php the_permalink(); ?>"><?php echo __('View More','sundance'); ?>+</a></div>
+								<br/>
+								<div class="parq-date"><img src="<?php echo get_stylesheet_directory_uri();?>/img/btn-arrow.png"> <?php echo get_the_date(); ?></div>
+							</div>
+						</div>
+					</div>
+					<?php
+					if( $i==3 || ( $i ==wp_count_posts() && wp_count_posts() < 3 ) ) { ?>
 						<div class="col-lg-4 grid-item " style="width:300px;">
 							<div class="catbox1">
 								<div class="indiv1">
 									<p><?php echo __('NUEYSTROS PROJECTS', 'sundance'); ?></p>
 									<ul class="list-unstyled">
-								<?php $args = array('hide_empty' => FALSE);
+								<?php
+											$args = array('exclude'=>$cid,'hide_empty' => FALSE,'parent' => 0);
 											$categories =get_categories( $args );
 											foreach($categories as $category){
-												echo '<li><a href="'.get_category_link( $category->term_id ).'">+&nbsp;'.$category->name.'</a></li>';
+													echo '<li><a href="'.get_category_link( $category->term_id ).'">+&nbsp;'.$category->name.'</a></li>';
 											}?>
 									</ul>
 								</div>
+							</div>
 						</div>
-					</div>
-					<?php } ?>
-					<div class="col-lg-4 grid-item " style="width:300px;">
-					<div  class="parq-box">
-						<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('full', array('class' => 'img-responsive parq-img')); ?></a>
-						<div class="parq-info">
-							<a href="<?php the_permalink(); ?>"><span class="parq-title"> <?php	the_title(); ?></span></a>
-							<div class="parq-content"><?php the_excerpt(); ?> <a class="more-link" href="<?php the_permalink(); ?>"><?php echo __('View More','sundance'); ?>+</a></div>
-							<br/>
-							<div class="parq-date"><img src="<?php echo get_stylesheet_directory_uri();?>/img/btn-arrow.png"> <?php echo get_the_date(); ?></div>
-						</div>
-					</div>
-					</div>
-				<?php
+					<?php }
+
 				$i++;
 				endwhile; ?>
 			</div>
