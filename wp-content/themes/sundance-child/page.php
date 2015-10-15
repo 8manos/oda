a64dd10d933ed8c6baa66da2408278c8995aac8f
sunny <sunny@runwalsoft.com>
2015-10-15 12:14:33 +0530

    modified for who we are page

--- wp-content/themes/sundance-child/page.php
+++ wp-content/themes/sundance-child/page.php
@@ -5,7 +5,7 @@ get_header(); ?>
 		<?php the_post_thumbnail( 'full', array('class' => 'img-responsive ban-image'));  ?>
 		<div class="events-middle">
 			<div class="events-table container">
-					<h1><?php echo __('ya viene', 'sundance'); ?></h1>
+<!-- 					<h1><?php echo __('ya viene', 'sundance'); ?></h1> -->
 					<?php $data=get_page( get_the_ID());
 					echo $data->post_content;
 					?>
@@ -18,21 +18,23 @@ get_header(); ?>
 			<?php	
 				$i=0;
 				$mypages = get_pages( array( 'child_of' => $post->ID, 'parent' => $post->ID ) );
-            foreach( $mypages as $page ) {      
-						if($i==0)
-						$content = $page->post_content;
-							if ( ! $content ) // Check for empty page
-                continue;
+				//if($mypages!="")
+				
+				foreach( $mypages as $page ) {      
+					if($i==0) {
+					$content = $page->post_content;
+						if ( ! $content ) // Check for empty page
+							continue;
 
-            $content = apply_filters( 'the_content', $content );
-        ?>
-				<div class="row">
-					<div class="col-lg-4 event-abt-title text-center"><?php echo $page->post_title; ?></div>
-					<div class="col-lg-8 event-abtus"><?php echo $content; ?></div>
-				</div>
-				<?php
-				}
-				$i++;
+					$content = apply_filters( 'the_content', $content );
+					?>
+					<div class="row">
+						<div class="col-lg-4 event-abt-title text-center"><?php echo $page->post_title; ?></div>
+						<div class="col-lg-8 event-abtus"><?php echo $content; ?></div>
+					</div>
+					<?php
+					}
+					$i++;
 				} ?>
 		</div>
 	</div>		
