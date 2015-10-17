a22ec29d337428033a7f31341833a54dec72684e
sunny <sunny@runwalsoft.com>
2015-10-17 13:43:08 +0530

    change shortcode as per serer shortcode

--- wp-content/themes/sundance-child/archive-customproduct.php
+++ wp-content/themes/sundance-child/archive-customproduct.php
@@ -48,7 +48,7 @@ get_header(); ?>
                $currentlang = get_bloginfo('language');
               if($currentlang=="en-US"):
               ?>
-           <div><?php echo do_shortcode("[contact-form-7 id='205' title='eco tienda form']"); ?></div>
+          <div><?php echo do_shortcode("[contact-form-7 id='329' title='eco tienda form-en']"); ?></div>
            <?php else: ?>
            <div><?php echo do_shortcode("[contact-form-7 id='186' title='eco tienda form']"); ?></div>
            <?php endif; ?>
